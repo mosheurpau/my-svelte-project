6b1c27ad4e94826f0304ba22653cc6356748df76
mosheurpau <mosheur.pau@gamil.com>
2024-06-13 13:08:30 +0600

    practice state

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  let count = 0;
+</script>
+
+<h1>Practice State</h1>
+<main>
+  <div>
+    <p>Counter value is: {count}</p>
+
+    <button on:click={() => count++}>Incriment</button>
+    <button on:click={() => count--}>Decriment</button>
+  </div>
+</main>
